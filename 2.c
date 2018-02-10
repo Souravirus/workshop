9363dd7e1900951dd96872b7d139e9024fda63b3
coolanmol <anmolgupta9558@gmail.com>
2018-02-10 13:00:58 +0530

    Added

---
+++ 2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf("The size of char %ld\n",sizeof('3'));
+	printf("the sizeof string %ld\n",sizeof("3"));
+    printf("The size of int %ld\n",sizeof(3));
+return 0;
+}   
